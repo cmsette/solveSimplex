13dc8666922a20d9489a67f9b97e16468254bfdd
Carla Sette <carlasette@Carlas-MacBook-Air.local>
2021-06-10 00:47:42 -0700

    Matrix input

--- cont3Classify.R
+++ cont3Classify.R
@@ -3,24 +3,27 @@
 # Sink (all -), Source (all +), Saddle (mix of - and +): Saddle 1-3 denotes number of positive eigen values
 # NaN(?) (any is NaN, divided by 0), Inconclusive (2nd smallest is 0), Validation error (test function validation steps were > 1e -14, i.e. not 0)
 # No eq (no equilibrium internal to simplex)
+# Error1: failed equilibrium calculation validation step in Test functions (may be resolved by increasing value of "zero" var)
+# Error2: projection matrix cannot be calculated
+# Error3: no zero eigenvalue calculated for projection matrix
 cont3Classify<-function(seq, W, zero){
-  if(is.na(seq[4])==T){type<-c("Validation error")}
-  else if(seq[4]==0){type<-c("No eq")}
-  else{W1<-W[1]; W2<-W[2]; W3<-W[3]; W4<-W[4]; W5<-W[5]; W6<-W[6]; W7<-W[7]; W8<-W[8]; W9<-W[9] 
+  if(is.na(seq[4])==T){type<-c("Error1")}
+  else if(seq[4]==0){type<-c("None")}
+  else{W1<-W[1,1]; W2<-W[1,2]; W3<-W[1,3]; W4<-W[2,1]; W5<-W[2,2]; W6<-W[2,3]; W7<-W[3,1]; W8<-W[3,2]; W9<-W[3,3] 
   r <- seq[1]; p <- seq[2]; s <- seq[3]																		#Defines eq
   P <- matrix( c( (2/3),(-1/3),(-1/3), (-1/3),(2/3),(-1/3), (-1/3),(-1/3),(2/3) ), ncol=3, byrow=T)			#Identity matrix
   #Calculates Jacobian matrix expressions for r: ∆r expression derived by r, p, s, respectively
-  Jac1<-as.expression(D(expression(r*((r*W1)+(p*W2)+(s*W3)) - r*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) 					+ s*((r*W3)+(p*W6)+(s*W9)) ) ), "r"))
-  Jac2<-as.expression(D(expression(r*((r*W1)+(p*W2)+(s*W3)) - r*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) 					+ s*((r*W3)+(p*W6)+(s*W9)) ) ), "p"))
-  Jac3<-as.expression(D(expression(r*((r*W1)+(p*W2)+(s*W3)) - r*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) 					+ s*((r*W3)+(p*W6)+(s*W9)) ) ), "s"))
+  Jac1<-as.expression(D(expression(r*((r*W1)+(p*W2)+(s*W3)) - r*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) + s*((r*W3)+(p*W6)+(s*W9)) ) ), "r"))
+  Jac2<-as.expression(D(expression(r*((r*W1)+(p*W2)+(s*W3)) - r*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) + s*((r*W3)+(p*W6)+(s*W9)) ) ), "p"))
+  Jac3<-as.expression(D(expression(r*((r*W1)+(p*W2)+(s*W3)) - r*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) + s*((r*W3)+(p*W6)+(s*W9)) ) ), "s"))
   #Calculates Jacobian matrix expressions for p: ∆p expression derived by r, p, s, respectively
-  Jac4<-as.expression(D(expression(p*((r*W4)+(p*W5)+(s*W6)) - p*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8))					 	+ s*((r*W3)+(p*W6)+(s*W9)) ) ), "r"))
-  Jac5<-as.expression(D(expression(p*((r*W4)+(p*W5)+(s*W6)) - p*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8))					 	+ s*((r*W3)+(p*W6)+(s*W9)) ) ), "p"))
-  Jac6<-as.expression(D(expression(p*((r*W4)+(p*W5)+(s*W6)) - p*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8))					 	+ s*((r*W3)+(p*W6)+(s*W9)) ) ), "s"))
+  Jac4<-as.expression(D(expression(p*((r*W4)+(p*W5)+(s*W6)) - p*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) + s*((r*W3)+(p*W6)+(s*W9)) ) ), "r"))
+  Jac5<-as.expression(D(expression(p*((r*W4)+(p*W5)+(s*W6)) - p*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) + s*((r*W3)+(p*W6)+(s*W9)) ) ), "p"))
+  Jac6<-as.expression(D(expression(p*((r*W4)+(p*W5)+(s*W6)) - p*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) + s*((r*W3)+(p*W6)+(s*W9)) ) ), "s"))
   #Calculates Jacobian matrix expressions for s: ∆s expression derived by r, p, s, respectively
-  Jac7<-as.expression(D(expression(s*((r*W7)+(p*W8)+(s*W9)) - s*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8))					 	+ s*((r*W3)+(p*W6)+(s*W9)) ) ), "r"))
-  Jac8<-as.expression(D(expression(s*((r*W7)+(p*W8)+(s*W9)) - s*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8))					 	+ s*((r*W3)+(p*W6)+(s*W9)) ) ), "p"))
-  Jac9<-as.expression(D(expression(s*((r*W7)+(p*W8)+(s*W9)) - s*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8))					 	+ s*((r*W3)+(p*W6)+(s*W9)) ) ), "s"))
+  Jac7<-as.expression(D(expression(s*((r*W7)+(p*W8)+(s*W9)) - s*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) + s*((r*W3)+(p*W6)+(s*W9)) ) ), "r"))
+  Jac8<-as.expression(D(expression(s*((r*W7)+(p*W8)+(s*W9)) - s*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) + s*((r*W3)+(p*W6)+(s*W9)) ) ), "p"))
+  Jac9<-as.expression(D(expression(s*((r*W7)+(p*W8)+(s*W9)) - s*( r*((r*W1)+(p*W4)+(s*W7)) + p*((r*W2)+(p*W5)+(s*W8)) + s*((r*W3)+(p*W6)+(s*W9)) ) ), "s"))
   #Inputs payoff matrix and eq values into expression, saves solutions as Jacobian matrix (Jac)
   e1 <- environment()
   Jac<-matrix(nrow=3, ncol=3, byrow=T)
