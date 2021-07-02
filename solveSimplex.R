acac7a6cf4ccab18e91a4fa8171c008dbeddbaf5
Carla Sette <66714242+cmsette@users.noreply.github.com>
2021-07-02 02:18:35 -0700

    updated solver

--- solveSimplex.R
+++ solveSimplex.R
@@ -3,89 +3,49 @@
 # gentime: defines replicator dynamics, "continuous" or "discrete" time steps
 # zero=1e-14: uses a value of 1e-14 instead of 0 in validation steps
 # sig_dig=7: default prints output with 7 significant figures
-# deconstruct=T: output includes independednt analysis of face games
+# colnames=c("R", "P", "S", "L", "K"): user can change variable names displayed
+# deconstruct=T: output includes analysis of face games as independent games
 
-solveSimplex<-function(W, gen_time="continuous", zero=1e-14, sig_dig=7, deconstruct=T){
-  options(warn=-1)
-  if(length(W) == 4){
-    W_out<-matrix(W, ncol=2, byrow=T); rownames(W_out)<-colnames(W_out)<-c("R", "P")
-    table_out<-table2(W, gen_time, zero, sig_dig, "RP")
-    return(list(W=W_out, gen_time=gen_time, table=table_out))
+solveSimplex <- function(W, gen_time="continuous", zero=1e-14, sig_dig=4, names=c("R", "P", "S", "L", "K"), deconstruct=T, debug=F){
+  # tests for numerical W, puts W in matrix format, check variable names vector length
+  if(sqrt(length(W)) %in% c(2,3,4,5)){
+    if(class(W)[1] == "numeric" && sapply(W, FUN = function(x) is.numeric(x)==T)){Wmatrix = matrix(W, nrow=sqrt(length(W)), byrow=T)} 
+    else if(class(W)[1] == "matrix" && mapply(W, FUN = function(x) is.numeric(x)==T)){Wmatrix = W} 
+    else{stop("non-numeric payoff values")}
+  } else{stop("simplex dimension ")}
+  if(length(names) < sqrt(length(W))){names=c("R","P","S","L","K")[1:(sqrt(length(W))-length(names))]}
+  if(length(names) > sqrt(length(W))){names=names[1:sqrt(length(W))]}
+  # analyzes game by number of competitors  
+  if(sqrt(length(W)) == 2){
+    table_out <- table2(Wmatrix, gen_time, zero, sig_dig, names)
+    if(debug==F){ if(deconstruct==T){return(table_out[-c(2,4)])}else{return(table_out[-c(2,4)])} }
+    else{ if(deconstruct==T){return(table_out[-2])}else{return(table_out[-2])} }
   }
-  else if(length(W) == 9){
-    W_out<-matrix(W, ncol=3, byrow=T); rownames(W_out)<-colnames(W_out)<-c("R", "P", "S")
-    if(deconstruct == T){
-      table_out<-table3(W, gen_time, zero, sig_dig)
-      table_out_RP<-table2(W[c(1,2, 4,5)], gen_time, zero, sig_dig, "RP")
-      table_out_RS<-table2(W[c(1,3, 7,9)], gen_time, zero, sig_dig, "RS")
-      table_out_PS<-table2(W[c(5,6, 8,9)], gen_time, zero, sig_dig, "PS")
-      table_out_2<-rbind(table_out_RP, table_out_RS, table_out_PS)
-      colnames(table_out_2)<-c("S1","S2","Equilibrium")
-      return(list(W=W_out, gen_time=gen_time, table=table_out, edges=table_out_2))
-    }else{
-      table_out<-table3(W, gen_time, zero, sig_dig)
-      return(list(W=W_out, gen_time=gen_time, table=table_out))
-    }
+  if(sqrt(length(W)) == 3){
+    table_out <- table3(Wmatrix, gen_time, zero, sig_dig, names)
+    if(debug==F){ 
+      if(deconstruct==T){return(c(table_out[[1]][1], table_out[[1]][3], table_out[2]))}
+      else{return(c(table_out[[1]][1], table_out[[1]][3]))} }
+    else{ 
+      if(deconstruct==T){return(c(table_out[[1]][1], table_out[[1]][3], table_out[2], table_out[[3]]))}
+      else{return(c(table_out[[1]][1], table_out[[1]][3], table_out[[3]][1]))} }
   }
-  else if(length(W) == 16){
-    W_out<-matrix(W, ncol=4, byrow=T); rownames(W_out)<-colnames(W_out)<-c("R", "P", "S", "L")
-    if(deconstruct == T){
-      table_out<-table4(W, gen_time, zero, sig_dig)
-      table_out_RPS<-table3(W[c(1,2,3, 5,6,7, 9,10,11)], gen_time, zero, sig_dig, "RPS")
-      table_out_RPL<-table3(W[c(1,2,4, 5,6,8, 13,14,16)], gen_time, zero, sig_dig, "RPL")
-      table_out_RSL<-table3(W[c(1,3,4, 9,11,12, 13,15,16)], gen_time, zero, sig_dig, "RSL")
-      table_out_PSL<-table3(W[c(6,7,8, 10,11,12, 14,15,16)], gen_time, zero, sig_dig, "PSL")
-      table_out_RP<-table2(W[c(1,2, 5,6)], gen_time, zero, sig_dig, "RP")
-      table_out_RS<-table2(W[c(1,3, 9,11)], gen_time, zero, sig_dig, "RS")
-      table_out_RL<-table2(W[c(1,4, 13,16)], gen_time, zero, sig_dig, "RL")
-      table_out_PS<-table2(W[c(6,7, 10,11)], gen_time, zero, sig_dig, "PS")
-      table_out_PL<-table2(W[c(6,8, 14,16)], gen_time, zero, sig_dig, "PL")
-      table_out_SL<-table2(W[c(11,12, 15,16)], gen_time, zero, sig_dig, "SL")
-      table_out_2<-rbind(table_out_RP, table_out_RS, table_out_RL, table_out_PS, table_out_PL, table_out_SL)
-      colnames(table_out_2)<-c("S1","S2","Equilibrium")
-      return(list(W=W_out, gen_time=gen_time, table=table_out, RPS=table_out_RPS, RPL=table_out_RPL, RSL=table_out_RSL, 						PSL=table_out_PSL, edges=table_out_2))
-    }else{
-      table_out<-table4(W, gen_time, zero, sig_dig)
-      return(list(W=W_out, gen_time=gen_time, table=table_out))
-    }
+  if(sqrt(length(W)) == 4){
+    table_out <- table4(Wmatrix, gen_time, zero, sig_dig, names)
+    if(debug==F){ 
+      if(deconstruct==T){return(c(table_out[[1]][1], table_out[[1]][3], table_out[2]))}
+      else{return(c(table_out[[1]][1], table_out[[1]][3]))} }
+    else{ 
+      if(deconstruct==T){return(c(table_out[[1]][1], table_out[[1]][3], table_out[2], table_out[[3]]))}
+      else{return(c(table_out[[1]][1], table_out[[1]][3], table_out[[3]][1]))} }
   }
-  else if(length(W) == 25){
-    W_out<-matrix(as.numeric(W), ncol=5, byrow=T); rownames(W_out)<-colnames(W_out)<-c("R", "P", "S", "L", "K")
-    if(deconstruct == T){
-      table_out<-table5(W, gen_time, zero, sig_dig)
-      table_out_RPSL<-table4(W[c(1,2,3,4, 6,7,8,9, 11,12,13,14, 16,17,18,19)], gen_time, zero, sig_dig, "RPSL")
-      table_out_RPSK<-table4(W[c(1,2,3,5, 6,7,8,10, 11,12,13,15, 21,22,23,25)], gen_time, zero, sig_dig, "RPSK")
-      table_out_RPLK<-table4(W[c(1,2,4,5, 6,7,9,10, 16,17,19,20, 21,22,24,25)], gen_time, zero, sig_dig, "RPLK")
-      table_out_RSLK<-table4(W[c(1,3,4,5, 11,13,14,15, 16,18,19,20, 21,23,24,25)], gen_time, zero, sig_dig, "RSLK")
-      table_out_PSLK<-table4(W[c(7,8,9,10, 12,13,14,15, 17,18,19,20, 22,23,24,25)], gen_time, zero, sig_dig, "PSLK")
-      table_out_RPS<-table3(W[c(1,2,3, 6,7,8, 11,12,13)], gen_time, zero, sig_dig, "RPS")
-      table_out_RPL<-table3(W[c(1,2,4, 6,7,9, 16,17,19)], gen_time, zero, sig_dig, "RPL")
-      table_out_RPK<-table3(W[c(1,2,5, 6,7,10, 21,22,25)], gen_time, zero, sig_dig, "RPK")
-      table_out_RSL<-table3(W[c(1,3,4, 11,13,14, 16,18,19)], gen_time, zero, sig_dig, "RSL")
-      table_out_RSK<-table3(W[c(1,3,5, 11,13,15, 21,23,25)], gen_time, zero, sig_dig, "RSK")
-      table_out_RLK<-table3(W[c(1,4,5, 16,19,20, 21,24,25)], gen_time, zero, sig_dig, "RLK")
-      table_out_PSL<-table3(W[c(7,8,9, 12,13,14, 17,18,19)], gen_time, zero, sig_dig, "PSL")
-      table_out_PSK<-table3(W[c(7,8,10, 12,13,15, 22,23,25)], gen_time, zero, sig_dig, "PSK")
-      table_out_PLK<-table3(W[c(7,9,10, 17,19,20, 22,24,25)], gen_time, zero, sig_dig, "PLK")
-      table_out_SLK<-table3(W[c(13,14,15, 18,19,20, 23,24,25)], gen_time, zero, sig_dig, "SLK")
-      table_out_RP<-table2(W[c(1,2, 6,7)], gen_time, zero, sig_dig, "RP")
-      table_out_RS<-table2(W[c(1,3, 11,13)], gen_time, zero, sig_dig, "RS")
-      table_out_RL<-table2(W[c(1,4, 16,19)], gen_time, zero, sig_dig, "RL")
-      table_out_RK<-table2(W[c(1,5, 21,25)], gen_time, zero, sig_dig, "RK")
-      table_out_PS<-table2(W[c(7,8, 12,13)], gen_time, zero, sig_dig, "PS")
-      table_out_PL<-table2(W[c(7,9, 17,19)], gen_time, zero, sig_dig, "PL")
-      table_out_PK<-table2(W[c(7,10, 22,25)], gen_time, zero, sig_dig, "PK")
-      table_out_SL<-table2(W[c(13,14, 18,19)], gen_time, zero, sig_dig, "SL")
-      table_out_SK<-table2(W[c(13,15, 23,25)], gen_time, zero, sig_dig, "SK")
-      table_out_LK<-table2(W[c(19,20, 24,25)], gen_time, zero, sig_dig, "LK")
-      table_out_2<-rbind(table_out_RP, table_out_RS, table_out_RL, table_out_RK, table_out_PS, table_out_PL, table_out_PK, 						table_out_SL, table_out_SK, table_out_LK)
-      colnames(table_out_2)<-c("S1","S2","Equilibrium")
-      return(list(W=W_out, gen_time=gen_time, table=table_out, RPSL=table_out_RPSL, 	RPSK=table_out_RPSK, RPLK=table_out_RPLK, 					RSLK=table_out_RSLK, PSLK=table_out_PSLK, RPS=table_out_RPS, RPL=table_out_RPL, RPK=table_out_RPK, RSL=table_out_RSL, 					RSK=table_out_RSK, RLK=table_out_RLK, PSL=table_out_PSL, PSK=table_out_PSK, PLK=table_out_PLK, SLK=table_out_SLK, 						edges=table_out_2))
-    }else{
-      table_out<-table5(W, gen_time, zero, sig_dig)
-      return(list(W=W_out, gen_time=gen_time, table=table_out))
-    }
+  if(sqrt(length(W)) == 5){
+    table_out <- table5(Wmatrix, gen_time, zero, sig_dig, names)
+    if(debug==F){ 
+      if(deconstruct==T){return(c(table_out[[1]][1], table_out[[1]][3], table_out[2]))}
+      else{return(c(table_out[[1]][1], table_out[[1]][3]))} }
+    else{ 
+      if(deconstruct==T){return(c(table_out[[1]][1], table_out[[1]][3], table_out[2], table_out[[3]]))}
+      else{return(c(table_out[[1]][1], table_out[[1]][3], table_out[[3]][1]))} }
   }
-  else{print("Error in simplex dimension")}
-  options(warn=0)
 }
